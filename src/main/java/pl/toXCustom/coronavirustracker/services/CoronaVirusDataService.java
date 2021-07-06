867fe5170c19a1cc8a917ef1eb0d12d9966233ae
toXCustom <pawel89@gmail.com>
2021-07-07 00:53:25 +0200

    Starting to work on the csv

--- src/main/java/pl/toXCustom/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/pl/toXCustom/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,27 +1,47 @@
 package pl.toXCustom.coronavirustracker.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import pl.toXCustom.coronavirustracker.models.LocationStats;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CoronaVirusDataService {
 
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+    private List<LocationStats> allStats = new ArrayList<>();
+
     @PostConstruct
+    @Scheduled(cron = "* * 1 * * *") //@Scheduled() run on regular basis the App
     public void fetchVirusData() throws IOException, InterruptedException {
+        List<LocationStats> newStats = new ArrayList<>();
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(VIRUS_DATA_URL))
                 .build();
 
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(httpResponse.body());
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+
+        //Apache Commons CSV
+        Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(csvBodyReader);
+
+        for (CSVRecord record : records) {
+            String state = record.get("Province/State");
+            String country = record.get("Country/Region");
+            System.out.println(state + " " + country);
+        }
     }
 }
